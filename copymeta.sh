e1c342e62390f0a5027c1108e4b0b016e2419dd0
ardeshir <ardeshir.org@gmail.com>
2017-06-13 20:05:51 +0000

    fixed bug

--- copymeta.sh
+++ copymeta.sh
@@ -4,14 +4,24 @@ declare DIR="/ltapps/initscripts"
 
 copy_current_uniq() {
 cat "current_application_metadata"    |  uniq >> "$DIR/application_metadata_t"
+sort -u "$DIR/application_metadata_t" |  uniq >  "$DIR/application_metadata"
+# clean up
+rm "$DIR/application_metadata_t"
+}
+
+copy_appmeta_uniq() {
+cat "current_application_metadata"    |  uniq >> "$DIR/application_metadata"
+sort -u "$DIR/application_metadata"   |  uniq >  "$DIR/application_metadata_t" 
 sort -u "$DIR/application_metadata_t" |  uniq >  "$DIR/application_metadata" 
+# clean up
+rm "$DIR/application_metadata_t"
 }
 
 if [[ -d "$DIR" && -x "$DIR" ]]; then 
 printf "Initscrtipt directory exists...\n" 
      if [[ -f "$DIR/application_metadata" ]]; then
        printf "Old $DIR/application_metadata exists...\n"
-       copy_current_uniq
+       copy_appmeta_uniq
      else 
      printf "Coping current to application_metadata"
      cp "current_application_metadata" "$DIR/application_metadata"
@@ -25,6 +35,5 @@ else
  # exit 0
 fi
 
-# clean up
-rm "$DIR/application_metadata_t" 
+ 
 exit 0
